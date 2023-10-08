2898eeed49d134c5bdcc08e3ccff252a485b92e1
Nathan Jones <natmax.jones@gmail.com>
2023-10-08 19:15:12 +0200

    Add wrapper for image-only transforms
    (Needs testing)

--- fundus_oct_challenge/transforms/oct_transforms.py
+++ fundus_oct_challenge/transforms/oct_transforms.py
@@ -106,6 +106,63 @@ class RandomScaleJoint(object):
         return "RandomScale"
 
 
+# --- Image only transformations --- #
+
+
+class ColorJitter(object):
+    def __init__(self, brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1):
+        self.brightness = brightness
+        self.contrast = contrast
+        self.saturation = saturation
+        self.hue = hue
+
+    def __call__(self, img, mask):
+        new_img = T.ColorJitter(
+            brightness=self.brightness,
+            contrast=self.contrast,
+            saturation=self.saturation,
+            hue=self.hue,
+        )(img)
+
+        return new_img, mask
+
+    def __str__(self):
+        return "ColorJitter"
+
+
+class GaussianBlur(object):
+    def __init__(self, kernel_size=3, sigma=(0.1, 2.0), p=0.5):
+        self.kernel_size = kernel_size
+        self.sigma = sigma
+        self.p = p
+
+    def __call__(self, img, mask):
+        new_img = T.RandomApply(
+            [T.GaussianBlur(kernel_size=self.kernel_size, sigma=self.sigma)], p=self.p
+        )(img)
+
+        return new_img, mask
+
+    def __str__(self):
+        return "GaussianBlur"
+
+
+class Normalize(object):
+    def __init__(self, mean=[0.485], std=[0.229]):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, img, mask):
+        new_img = T.Normalize(mean=self.mean, std=self.std)(img)
+
+        return new_img, mask
+
+    def __str__(self):
+        return "Normalize"
+
+    # ... Add any other image-only transformations
+
+
 # -------------------------------- Function --------------------------------- #
 
 
@@ -134,15 +191,15 @@ def get_transforms(mode="train"):
 
     # Image-only augmentations
     image_transforms_train = [
-        T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        T.RandomApply([T.GaussianBlur(3, sigma=(0.1, 2.0))], p=0.5),
+        ColorJitter(),
+        GaussianBlur(),
         # ... Add any other image-only transformations for training mode
-        (T.Normalize(mean=[0.485], std=[0.229]),),
+        Normalize(),
     ]
 
     image_transforms_val = [
         # ... Add any other image-only transformations for validation/test mode
-        (T.Normalize(mean=[0.485], std=[0.229]),)
+        Normalize(),
     ]
 
     # Compose based on mode
@@ -158,6 +215,8 @@ def get_transforms(mode="train"):
 
 # -------------------------------- Testing --------------------------------- #
 
+# Code for visual testing of the transformations
+
 # if __name__ == "__main__":
 #     from torchvision.io import read_image
 #     from pathlib import Path
@@ -165,22 +224,22 @@ def get_transforms(mode="train"):
 #     import os
 #     import matplotlib.pyplot as plt
 
-#     image_path_train = Path("../../data/GOALS/Train/Image")
-#     mask_path_train = Path("../../data/GOALS/Train/Layer_Masks")
+#     image_path_train = Path("data/GOALS/Train/Image")
+#     mask_path_train = Path("data/GOALS/Train/Layer_Masks")
 
 #     train_imgs = np.random.choice(list(image_path_train.iterdir()), size=1)
 #     img = read_image(str(train_imgs[0]))
 #     mask = read_image(str(mask_path_train / Path(train_imgs[0].name)))
 
 #     transformations = [
-#         RandomHorizontalFlipJoint(p=1),
-#         RandomRotationJoint(),
-#         VerticalShiftJoint(),
-#         RandomScaleJoint(),
-#     ]
+#             RandomHorizontalFlipJoint(p=1),
+#             RandomRotationJoint(),
+#             VerticalShiftJoint(),
+#             RandomScaleJoint()
+#         ]
 
 #     n_columns = 5
-#     n_rows = 2 * int((len(transformations) + 1) / n_columns)
+#     n_rows = int((len(transformations) + 1) / n_columns)
 #     height = max(3, n_rows * 3)
 #     width = 15
 
@@ -190,15 +249,10 @@ def get_transforms(mode="train"):
 #     plt.title("Original image")
 
 #     for i, transform in enumerate(transformations):
-#         transformed_img, transformed_mask = transform(img, mask)
+#         transformed_img, _ =  transform(img, mask)
 
-#         # Plot image and mask
 #         plt.subplot(n_rows, n_columns, i + 2)
 #         plt.imshow(transformed_img.permute(1, 2, 0).numpy())
-#         plt.title(transform.__str__())
-
-#         plt.subplot(n_rows, n_columns, i + 2 + len(transformations) + 1)
-#         plt.imshow(transformed_mask.permute(1, 2, 0).numpy())
-#         plt.title(transform.__str__())
+#         plt.title(str(transform))
 
 #     plt.show()
